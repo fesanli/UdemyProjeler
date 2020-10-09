6889a834173f82d2d2222d835d2b5158ec5e6e29
Ferhat <ferhatsanli95@hotmail.com>
2020-10-09 15:34:06 +0300

    -OOP package bug fixes -SampleInheritance

--- src/main/kotlin/Projeler/Inheritance/SampleInheritance.kt
+++ src/main/kotlin/Projeler/Inheritance/SampleInheritance.kt
@@ -1,2 +1,61 @@
 package Projeler.Inheritance
 
+open class Person (isim:String , yas:Int, isMan:Boolean) {
+    /*
+    *   Language Translations:
+    *   isim = name
+    *   yas = age
+    *   Ogretmen = Teacher
+    * */
+
+    var isim : String
+    var yas : Int
+    var isMan : Boolean
+
+    init {
+        println("init of person class triggered")
+        this.isim = isim
+        this.yas = yas
+        this.isMan = isMan
+    }
+
+    override fun toString(): String {
+        var cinsiyet = if (isMan) "Erkek" else "Kadin"
+        return "Isim: $isim, Yas: $yas, Cinsiyet: $cinsiyet"
+    }
+}
+class Ogretmen (isim:String, yas:Int, isMan: Boolean, brans:String) : Person (isim, yas, isMan){
+    var brans:String
+
+    init {
+        println("init of Ogretmen triggered")
+        this.brans = brans
+    }
+
+}
+
+
+fun main() {
+    var p1 : Person = Person("ferhat", 25, true)
+    println("toString: ${p1.toString()}")
+    println("just var name: ${p1}")
+
+    var t1:Ogretmen = Ogretmen("anna", 37, false, "Acting")
+    println(t1)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
